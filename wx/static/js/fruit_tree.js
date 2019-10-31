812da91310442c2e2fa1195a856de547feea60cd
jocelyn <961951661@qq.com>
2019-10-31 16:55:54 +0800

    111

--- wx/static/js/fruit_tree.js
+++ wx/static/js/fruit_tree.js
@@ -264,8 +264,6 @@ $(function(){
     $(document).on("click", ".js_steal_list a", function(){
         if($(this).hasClass("js_steal_btn")) {
             var fOpenId = $(this).data("val");
-            console.log($(this))
-            console.log(fOpenId)
 
             $(this).removeClass("js_steal_btn").siblings("i").remove()
             $(".fix-tool").animate({
@@ -362,7 +360,7 @@ $(function(){
                 signature = payConfig.signature;
 
                 wx.config({
-                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: appId, // 必填，公众号的唯一标识
                     timestamp: timestamp, // 必填，生成签名的时间戳
                     nonceStr: nonceStr, // 必填，生成签名的随机串
@@ -389,8 +387,8 @@ $(function(){
                     });
 
                     wx.updateAppMessageShareData({
-                        // title: payConfig.title,
-                        // desc: payConfig.description,
+                        title: payConfig.title,
+                        desc: payConfig.description,
                         link: payConfig.url,
                         imgUrl: payConfig.thumb,
                         trigger: function (res) {
